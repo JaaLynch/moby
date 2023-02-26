c548ba3d7fbca9101829d8574f2d923fc2630673
jaalynch <jaalynch@gmail.com>
2023-02-26 04:12:33 +0000

    volume instead of mount

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,3 @@
 #docker run -i -t -p 8888:8888 moby
-docker run -i -t -p 8888:8888  --mount  source=jl-vol, target=/home moby
+#docker run -i -t -p 8888:8888  --mount  source=jl-vol, target=/home moby
+docker run -i -t -p 8888:8888 -v jl-vol:/home moby
